b8becb091d9c2d53bc2224deb80858d1f027b2dd
andrewtaker <n0thingg@yandex.ru>
2023-12-18 03:05:32 +0300

    async rebuild

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,8 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.code.gson:gson:2.10")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
@@ -69,4 +70,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
 }
--- app/src/main/java/com/example/rv4fcm/MainActivity.kt
+++ app/src/main/java/com/example/rv4fcm/MainActivity.kt
@@ -3,55 +3,136 @@ package com.example.rv4fcm
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.rv4fcm.ui.theme.Rv4fcmTheme
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.ktx.messaging
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+import java.util.Locale
+import java.util.UUID
+
+
+const val PUSHEXPRESS_APP_ID: String = "9999-999999"
+
+// Local storage keys.
+const val FIREBASE_TOKEN: String = "fb_token"
+const val PUSHEXPRESS_ID: String = "pushex_id"
+const val IC_TOKEN: String = "icToken"
+
+// Log level.
+const val LOG_APP_INFO: String = "APP INFO"
+const val LOG_LOCAL_STORAGE: String = "LOCAL STORAGE"
+const val LOG_TESTING: String = "TESTING"
+
+// Endpoints.
+const val PUSHEXPRESS_BASE: String = "https://core.push.express/api/r/v2/apps"
+const val PUSHEXPRESS_INSTANCES: String = "$PUSHEXPRESS_BASE/$PUSHEXPRESS_APP_ID/instances"
+
+// TBA.
+val DEVICE_LANGUAGE = Locale.getDefault().language
 
 class MainActivity : ComponentActivity() {
+    // 1.1. Generate some local-persistent installation-token
+    private var icToken = UUID.randomUUID().toString()
+    var localStorageManager = LocalStorageManager(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getFirebaseToken()
-        setContent {
-            Rv4fcmTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        localStorageManager.clearLocalStorage()
+        // Save ic_id to local storage.
+        localStorageManager.saveItem(IC_TOKEN, icToken)
+        // Get FireBase token and save it to local storage. Key: fb_token.
+        getAndSaveFireBaseToken()
+        // Get IC ID and save it to local storage. Key: pushex_id.
+        getAndSavePushExID()
+        // Update app instance info
+        updateAppInstanceInfo()
     }
-    private fun getFirebaseToken() {
+    private fun getAndSaveFireBaseToken(){
         Firebase.messaging.token.addOnCompleteListener {
             if (it.isSuccessful) {
-                Log.d("qwe", "getFirebaseToken: token=${it.result}")
+                Log.d(LOG_APP_INFO,  "token=${it.result}")
+                // Save fb token to a local storage.
+                localStorageManager.saveItem(FIREBASE_TOKEN, it.result)
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Rv4fcmTheme {
-        Greeting("Android")
+    private fun getAndSavePushExID(){
+        val url = "https://core.push.express/api/r/v2/apps/$PUSHEXPRESS_APP_ID/instances"
+        val json = """
+    {
+        "ic_token": "$icToken"
     }
+""".trimIndent()
+        val body = json.toRequestBody("application/json".toMediaTypeOrNull())
+        val request = Request.Builder()
+            .url(url)
+            .header("Content-Type", "application/json")
+            .post(body)
+            .build()
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("RESPONSE ERROR", e.printStackTrace().toString())
+            }
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body?.string()
+                    Log.d(LOG_APP_INFO, responseBody!!)
+                    try {
+                        val jsonObject = JSONObject(responseBody)
+                        val id = jsonObject.getString("id")
+                        localStorageManager.saveItem(PUSHEXPRESS_ID, id)
+                        localStorageManager.getItem(PUSHEXPRESS_ID)
+                            ?.let { Log.d("PUSHEXPRESSID", it) }
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        })
+    }
+    private fun updateAppInstanceInfo(){
+        var icID = localStorageManager.getItem(PUSHEXPRESS_ID)
+        val url = "https://core.push.express/api/r/v2/apps/$PUSHEXPRESS_APP_ID/instances/$icID/info"
+        Log.d("INSTANCE URL", url)
+        val json = """
+    {
+        "transport_type": "fcm",
+        "transport_token": "$FIREBASE_TOKEN",
+    }
+""".trimIndent()
+        val body = json.toRequestBody("application/json".toMediaTypeOrNull())
+        val request = Request.Builder()
+            .url(url)
+            .header("Content-Type", "application/json")
+            .put(body)
+            .build()
+        val client = OkHttpClient()
+
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("RESPONSE ERROR", e.printStackTrace().toString())
+            }
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body?.string()
+                    Log.d(LOG_APP_INFO + "ASDADS", responseBody!!)
+                } else {
+                    response.body?.let { Log.d("TAHT DOESNT SEEM RIGHT", it.string()) }
+                }
+            }
+        })
+        Log.d("REQUEST BODY", request.body.toString())
+    }
+
+
 }
