720d90232b45cf869facf678f1c47ba6cbc52ca2
andrewtaker <n0thingg@yandex.ru>
2023-12-19 10:22:48 +0300

    current project, done to the point of sending basic user info. removed delay between calls and added infinite retry with exponential backoff algorithm (i think).

--- app/src/main/java/com/example/rv4fcm/MainActivity.kt
+++ app/src/main/java/com/example/rv4fcm/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.rv4fcm
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.telephony.TelephonyManager
 import android.util.Log
 import androidx.activity.ComponentActivity
 import com.google.firebase.ktx.Firebase
@@ -12,127 +15,160 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
-import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
 import java.util.Locale
+import java.util.Random
+import java.util.TimeZone
 import java.util.UUID
+import kotlin.math.pow
 
 
 const val PUSHEXPRESS_APP_ID: String = "9999-999999"
+
+// App preference filename.
+const val PREFERENCE_FILENAME: String = "app_preference"
 
 // Local storage keys.
 const val FIREBASE_TOKEN: String = "fb_token"
 const val PUSHEXPRESS_ID: String = "pushex_id"
 const val IC_TOKEN: String = "icToken"
 
-// Log level.
+// Log tag.
 const val LOG_APP_INFO: String = "APP INFO"
-const val LOG_LOCAL_STORAGE: String = "LOCAL STORAGE"
-const val LOG_TESTING: String = "TESTING"
-
-// Endpoints.
-const val PUSHEXPRESS_BASE: String = "https://core.push.express/api/r/v2/apps"
-const val PUSHEXPRESS_INSTANCES: String = "$PUSHEXPRESS_BASE/$PUSHEXPRESS_APP_ID/instances"
-
-// TBA.
-val DEVICE_LANGUAGE = Locale.getDefault().language
 
 class MainActivity : ComponentActivity() {
-    // 1.1. Generate some local-persistent installation-token
-    private var icToken = UUID.randomUUID().toString()
-    var localStorageManager = LocalStorageManager(this)
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var icToken: String
+    private val client = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        localStorageManager.clearLocalStorage()
-        // Save ic_id to local storage.
-        localStorageManager.saveItem(IC_TOKEN, icToken)
-        // Get FireBase token and save it to local storage. Key: fb_token.
-        getAndSaveFireBaseToken()
-        // Get IC ID and save it to local storage. Key: pushex_id.
-        getAndSavePushExID()
-        // Update app instance info
-        updateAppInstanceInfo()
-    }
-    private fun getAndSaveFireBaseToken(){
-        Firebase.messaging.token.addOnCompleteListener {
-            if (it.isSuccessful) {
-                Log.d(LOG_APP_INFO,  "token=${it.result}")
-                // Save fb token to a local storage.
-                localStorageManager.saveItem(FIREBASE_TOKEN, it.result)
+        sharedPreferences = getSharedPreferences(PREFERENCE_FILENAME, Context.MODE_PRIVATE)
+        icToken = sharedPreferences.getString(IC_TOKEN, "").toString()
+
+        // Check if uuid already exists in local storage.
+        if (icToken.isEmpty()) {
+            icToken = UUID.randomUUID().toString()
+            with (sharedPreferences.edit()) {
+                putString(IC_TOKEN, icToken)
+                apply()
             }
         }
+        Log.d(LOG_APP_INFO, "CURRENT UUID: $icToken")
+        // Get firebase token.
+        Firebase.messaging.token.addOnCompleteListener {
+            if (it.isSuccessful) {
+                with (sharedPreferences.edit()){
+                    putString(FIREBASE_TOKEN, it.result)
+                    apply()
+                }
+            }
+        }
+        getAppID()
+        updateAppInfo()
+        Log.d(LOG_APP_INFO, "firebase token ${sharedPreferences
+            .getString(FIREBASE_TOKEN, null)}")
+        Log.d(LOG_APP_INFO, "pushexpress id ${sharedPreferences
+            .getString(PUSHEXPRESS_ID, null)}")
+        Log.d(LOG_APP_INFO, "ic token ${sharedPreferences
+            .getString(IC_TOKEN, null)}")
     }
-    private fun getAndSavePushExID(){
-        val url = "https://core.push.express/api/r/v2/apps/$PUSHEXPRESS_APP_ID/instances"
-        val json = """
-    {
-        "ic_token": "$icToken"
-    }
-""".trimIndent()
-        val body = json.toRequestBody("application/json".toMediaTypeOrNull())
-        val request = Request.Builder()
-            .url(url)
-            .header("Content-Type", "application/json")
-            .post(body)
+    private fun getAppID() {
+        var sharedPreferences = getSharedPreferences(PREFERENCE_FILENAME, Context.MODE_PRIVATE)
+        val json = JSONObject()
+        json.put("ic_token", icToken)
+
+        val requestBody = json.toString()
+            .toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                var request = chain.request()
+                    .newBuilder()
+                    .post(requestBody)
+                    .build()
+                var response = chain.proceed(chain.request())
+                var tryCount = 0
+                while (!response.isSuccessful && tryCount < 10) {
+                    tryCount++
+                    val sleepTime = (2.0.pow(tryCount.toDouble()) + Random().nextInt(3000 - 1000) + 1000).toLong()
+                    Thread.sleep(sleepTime)
+                    response.close()
+
+                    response = chain.proceed(chain.request())
+                    request = request.newBuilder().build()
+                }
+                response
+            }
             .build()
-        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url("https://core.push.express/api/r/v2/apps/$PUSHEXPRESS_APP_ID/instances")
+            .build()
 
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
-                Log.e("RESPONSE ERROR", e.printStackTrace().toString())
+                e.printStackTrace()
             }
+
             override fun onResponse(call: Call, response: Response) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body?.string()
-                    Log.d(LOG_APP_INFO, responseBody!!)
-                    try {
-                        val jsonObject = JSONObject(responseBody)
-                        val id = jsonObject.getString("id")
-                        localStorageManager.saveItem(PUSHEXPRESS_ID, id)
-                        localStorageManager.getItem(PUSHEXPRESS_ID)
-                            ?.let { Log.d("PUSHEXPRESSID", it) }
-                    } catch (e: JSONException) {
-                        e.printStackTrace()
-                    }
+                val responseBody = response.body?.string()
+                val id = JSONObject(responseBody).getString("id")
+                with (sharedPreferences.edit()){
+                    putString(PUSHEXPRESS_ID, id)
+                    apply()
                 }
             }
         })
     }
-    private fun updateAppInstanceInfo(){
-        var icID = localStorageManager.getItem(PUSHEXPRESS_ID)
-        val url = "https://core.push.express/api/r/v2/apps/$PUSHEXPRESS_APP_ID/instances/$icID/info"
-        Log.d("INSTANCE URL", url)
-        val json = """
-    {
-        "transport_type": "fcm",
-        "transport_token": "$FIREBASE_TOKEN",
-    }
-""".trimIndent()
-        val body = json.toRequestBody("application/json".toMediaTypeOrNull())
-        val request = Request.Builder()
-            .url(url)
-            .header("Content-Type", "application/json")
-            .put(body)
-            .build()
-        val client = OkHttpClient()
 
+    private fun getDeviceCountry(context: Context): String {
+        try {
+            val telephonyManager = context
+                .getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            return telephonyManager.networkCountryIso
+        } catch (e: Exception) {
+            Log.e(LOG_APP_INFO, e.toString())
+        } finally {
+            return ""
+        }
+    }
+
+    private fun updateAppInfo() {
+        var sharedPreferences = getSharedPreferences(PREFERENCE_FILENAME, Context.MODE_PRIVATE)
+
+        val firebaseToken = sharedPreferences.getString(FIREBASE_TOKEN, null)
+        val icID = sharedPreferences.getString(PUSHEXPRESS_ID, null)
+        val deviceCountry = getDeviceCountry(this)
+        val deviceLanguage = Locale.getDefault().language
+        val deviceTimeZone = TimeZone.getDefault().rawOffset / 1000
+
+        val json = JSONObject()
+        json.put("transport_type", "fcm")
+        json.put("transport_token", firebaseToken)
+        json.put("platform_type", "android")
+        json.put("lang", deviceLanguage)
+        json.put("country", deviceCountry.uppercase())
+        json.put("tz_sec", deviceTimeZone)
+
+        val requestBody = json.toString()
+            .toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+
+        val request = Request.Builder()
+            .url("https://core.push.express/api/r/v2/apps/$PUSHEXPRESS_APP_ID/instances/$icID/info")
+            .put(requestBody)
+            .build()
 
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
-                Log.e("RESPONSE ERROR", e.printStackTrace().toString())
+                e.printStackTrace()
             }
+
             override fun onResponse(call: Call, response: Response) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body?.string()
-                    Log.d(LOG_APP_INFO + "ASDADS", responseBody!!)
-                } else {
-                    response.body?.let { Log.d("TAHT DOESNT SEEM RIGHT", it.string()) }
-                }
+                val responseBody = response.body?.string()
+                Log.d(LOG_APP_INFO, "update info response: $responseBody")
             }
         })
-        Log.d("REQUEST BODY", request.body.toString())
     }
-
-
 }
